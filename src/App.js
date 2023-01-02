24965695dd1982327ae43040b7e118f2cc3ae032
MoeGropp <112837865+MoeGropp@users.noreply.github.com>
2023-01-02 17:47:44 -0600

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
    <h1>Weather App Project</h1>
    <footer>
     This project was coded by Morgan Gropp and is {""}
@@ -11,6 +12,7 @@ function App() {
       open sourced on GitHub 
    </a>
    </footer>
+   </div>
     </div>
   );
 }

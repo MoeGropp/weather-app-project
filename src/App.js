ff95f6b2d60aef72c37b54c8a1fd727a7d8aa4a8
MoeGropp <112837865+MoeGropp@users.noreply.github.com>
2023-01-02 11:46:42 -0600

    Added github link to footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-   Weather App Project
+   <h1>Weather App Project</h1>
+   <footer>
+    This project was coded by Morgan Gropp and is {""}
+   <a href="https://github.com/MoeGropp/weather-app-project.git" target="_blank">
+      open sourced on GitHub 
+   </a>
+   </footer>
     </div>
   );
 }

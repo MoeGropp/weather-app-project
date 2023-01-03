0477fdf038f24a9ab23f86638d75e4bd93f204f2
MoeGropp <112837865+MoeGropp@users.noreply.github.com>
2023-01-02 18:18:01 -0600

    Started developing the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import React from "react";
 import './App.css';
+import Weather from "./Weather.js"
 
 function App() {
   return (
     <div className="App">
       <div className="container">
    <h1>Weather App Project</h1>
+   <Weather />
    <footer>
     This project was coded by Morgan Gropp and is {""}
    <a href="https://github.com/MoeGropp/weather-app-project.git" target="_blank">
---
+++ src/Weather.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather(){
+    return(
+        <div className="Weather">
+            <form>
+            <div className="row">
+            <div className="col-9">
+            
+                <input type="search" placeholder="Enter a city..." className="form-control"/>
+                </div>
+                <div className="col-3">
+                <input type="submit" value="Search" className="btn btn-primary"/>
+            
+            </div>
+            </div>
+            </form>
+        <h1>Oklahoma City, OK</h1>
+        <ul>
+            <li>Monday 10:00</li>
+            <li>Sunny</li>
+        </ul>
+        <div className="row">
+            <div className="col-6">
+              <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny"/>
+                
+                  67℉
+            </div>
+<div className="col-6">
+    <ul>
+        <li>Precipitation: 5%</li>
+        <li>Humidity: 73%</li>
+        <li>Wind: 11 mph</li>
+    </ul>
+</div>
+        </div>
+        </div>
+    )
+}

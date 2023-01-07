b88aab181d658755f97654666f901a0491c8a109
MoeGropp <112837865+MoeGropp@users.noreply.github.com>
2023-01-07 13:08:20 -0600

    Conversion
    I added a unit conversion for temperatures from celsius to fahrenheit and the other way around

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,22 +1,26 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props){
   return(
     <div className="WeatherInfo">
     <h1>{props.data.city}</h1>
             <ul>
-                <li><FormattedDate date={props.data.date} /></li>
-                <li className="text-capitalize">{props.data.description}</li>
+                <li>
+                    <FormattedDate date={props.data.date} />
+                </li>
+                <li className="text-capitalize">
+                    {props.data.description}
+                </li>
             </ul>
             <div className="row mt-3">
                 <div className="col-6">
                     <div>
-                    <WeatherIcon code={props.data.WeatherIcon} 
-                      alt={props.data.description} />
+                    <WeatherIcon code={props.data.WeatherIcon} />
                       </div>
-                   
+                   <WeatherTemperature />
                       <span className="temperature">{Math.round(props.data.temperature)}</span>
                       <span className="unit">℉</span>
                       

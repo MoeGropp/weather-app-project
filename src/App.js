3c9827dc9f13daddbaa099c429dd8cf219056edc
MoeGropp <112837865+MoeGropp@users.noreply.github.com>
2023-01-07 13:24:23 -0600

    Deleted the extra weather
    in the App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-   <Weather defaultCity ="New York City" />
-   <Weather />
+   <Weather  />
    <footer>
     This project was coded by Morgan Gropp and is {""}
    <a href="https://github.com/MoeGropp/weather-app-project.git" rel="noopener">

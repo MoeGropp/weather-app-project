dcc092634e1bd0bc465c8c488cdecce18dd736dd
MoeGropp <112837865+MoeGropp@users.noreply.github.com>
2023-01-10 16:09:27 -0600

    The start of the weather forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,8 +18,8 @@ export default function WeatherInfo(props){
             <div className="row mt-3">
                 <div className="col-6">
                     
-                    <div>
-                    <WeatherIcon code={props.data.icon} />
+                    <div >
+                    <WeatherIcon code={props.data.icon} size={52} />
                 
                       </div>
                    <WeatherTemperature celsius={props.data.temperature} />
